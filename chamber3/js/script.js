4aff124ae3b3e0d954565af3769316a604549439
Daniel Harris <harris.daniel.reed@gmail.com>
2023-05-18 01:30:39 -0600

    Cleanup

---
+++ chamber3/js/script.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburguerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburguerBtn');
+
+x.onclick = toggleMenu;
+
+document.querySelector('#©').textContent = new Date().getFullYear();
+
+const opciones = 
+{ weekday: 'long', 
+year: 'numeric', 
+month: 'numeric', 
+day: 'numeric' };
+
+document.querySelector('#dateMod').textContent = 
+new Date(Date.now()).toLocaleString();
+
+const opciones2 = 
+{ weekday: 'long', 
+year: 'numeric', 
+month: 'long', 
+day: 'numeric' };
+
+
+document.querySelector('#datetime').textContent = 
+new Date(Date.now()).toLocaleString('en-US',opciones2);
+
+
+
