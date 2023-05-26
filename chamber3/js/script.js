e239816771ccb64b5953b0be5af61fceda576a34
Daniel Harris <harris.daniel.reed@gmail.com>
2023-05-25 22:02:25 -0600

    benner updates

--- chamber3/js/script.js
+++ chamber3/js/script.js
@@ -28,10 +28,24 @@ day: 'numeric' };
 document.querySelector('#datetime').textContent = 
 new Date(Date.now()).toLocaleString('en-US',opciones2);
 
+// Script for the to display a banner on Mondays or Tuesdays only at the very 
+// top of the page that says "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m." 
+const banner = document.querySelector('#top-page-banner');
 
-  const banner = document.getElementById("banner");
-  const today = new Date().getDay();
-  if (today === 1 || today === 2) {
-    banner.style.display = "block";
-  }
+let dayOfWeek = new Date().getDay();
+
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    banner.style.display = 'block';
+}
+else {
+    banner.style.display = 'none';
+}
+
+// Script for closing the banner
+const closeBtn = document.querySelector('.bannerBtn');
+closeBtn.addEventListener('click', function() {
+    if (banner.style.display !== "none") {
+        banner.style.display = "none";
+    }
+})
 
