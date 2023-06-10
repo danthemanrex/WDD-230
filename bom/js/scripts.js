6f9419c5d27fac8240c4d1e0990756c802cb4e48
Daniel Harris <harris.daniel.reed@gmail.com>
2023-06-09 23:07:41 -0600

    JS updates

--- bom/js/scripts.js
+++ bom/js/scripts.js
@@ -1,34 +1,25 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('#myButton');
-const list = document.querySelector('.list');
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const output = document.querySelector(".list");
 
-button.onclick = function(){
-    let nameChapter = input.value;
+button.addEventListener('click', () => {
+    if (input.value != '' ) {
+        let li = document.createElement('li');
+        li.textContent = input.value;
 
-    if (nameChapter.trim() == ''){      // if the text is empty print an alert
-        alert('Input field is empty');
-        return true
+        let deletebutton = document.createElement
+        ('button');
+        deletebutton.textContent = '❌';
+
+        li.append(deletebutton);
+        output.append(li);
+        
+        deletebutton.addEventListener('click', function ()
+        {
+            output.removeChild(li);
+            input.focus;
+        });
+        input.value = '';
+        input.focus;
     }
-    
-    input.innerHTML= ''; 
-    
-    let li = document.createElement('li');
-    let span = document.createElement('span');
-    span.textContent = nameChapter;
-    let buttonli = document.createElement('button');
-    buttonli.textContent = '❌'
-
-    li.appendChild(span);
-    li.appendChild(buttonli);
-    list.appendChild(li);
-
-    buttonli.addEventListener('click', function(){ 
-        li.remove();
-    });
-
-}
-
-button.addEventListener('click', function() {
-    input.focus(); // focus the text selector into the input
-    input.value = ''; // eliminate the text after added
-})
+});
