744f7b3df1493c53ae4a5c530ec373aeae1c1e40
Daniel Harris <harris.daniel.reed@gmail.com>
2023-06-15 21:56:27 -0600

    combined page commit

---
+++ chamber/js/js/script.js
@@ -0,0 +1,53 @@
+function toggleMenu() {
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburguerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburguerBtn');
+
+x.onclick = toggleMenu;
+
+document.querySelector('#©').textContent = new Date().getFullYear();
+
+const opciones = 
+{ weekday: 'long', 
+year: 'numeric', 
+month: 'numeric', 
+day: 'numeric' };
+
+const opciones2 = 
+{ weekday: 'long', 
+year: 'numeric', 
+month: 'long', 
+day: 'numeric' };
+
+
+var datetimeElement = document.querySelector('#datetime');
+if (datetimeElement !== null) {
+  datetimeElement.textContent = new Date(Date.now()).toLocaleString('en-US',opciones2);
+}
+
+document.querySelector('#datetime').textContent = 
+new Date(Date.now()).toLocaleString('en-US',opciones2);
+
+// Script for the to display a banner on Mondays or Tuesdays only at the very 
+// top of the page that says "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m." 
+const banner = document.querySelector('#top-page-banner');
+
+let dayOfWeek = new Date().getDay();
+
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    banner.style.display = 'block';
+}
+else {
+    banner.style.display = 'none';
+}
+
+// Script for closing the banner
+const closeBtn = document.querySelector('.bannerBtn');
+closeBtn.addEventListener('click', function() {
+    if (banner.style.display !== "none") {
+        banner.style.display = "none";
+    }
+})
+
